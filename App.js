5ef53f102151198c639416e9cd88d1573a4a58b1
Ali Hesham <63455367+aliarfeen@users.noreply.github.com>
2025-11-13 08:33:23 +0200

    calling AppNavigator correctlly

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { Provider } from 'react-redux';
 import "./global.css";
+import AppNavigator from './src/navigation/AppNavigator';
 import store from './src/redux/store';
-import HomeScreen from './src/screens/HomeScreen';
+// import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
-      {/* <AppNavigator /> */}
+      {/* <HomeScreen /> */}
+      <AppNavigator />
     </Provider>
   );
 }
