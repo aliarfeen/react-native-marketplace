c54c39ab4779f41fff754390ade9ac72bcbeef91
Asmaa Adel <asmaghlab073@gmail.com>
2025-11-14 10:04:34 +0200

    Add toast notification to App.js

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { Provider } from 'react-redux';
 import "./global.css";
 import AppNavigator from './src/navigation/AppNavigator';
 import store from './src/redux/store';
-// import HomeScreen from './src/screens/HomeScreen';
+import Toast from "react-native-toast-message";
+import { CustomToast } from "./src/components/CustomToast";
 
 export default function App() {
   return (
     <Provider store={store}>
-      {/* <HomeScreen /> */}
       <AppNavigator />
+        <Toast config={CustomToast} />
     </Provider>
   );
 }
