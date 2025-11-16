7ba3911094e5d5ec452a8527c0e4ab01f0423d46
Asmaa Adel <asmaghlab073@gmail.com>
2025-11-16 06:39:10 +0200

    replace menu with logo

--- src/components/Home/NavBar.js
+++ src/components/Home/NavBar.js
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import { Pressable, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Navbar = () => {
@@ -10,7 +10,9 @@ const Navbar = () => {
     <View className="w-full flex-row items-center justify-between px-4 py-4">
 
       <Pressable className="h-12 w-12 rounded-full bg-gray-100 flex items-center justify-center">
-        <Ionicons name="menu" size={26} color="#000" />
+           <Text style={{ fontSize: 30, fontWeight: "500" ,marginLeft:80 ,color:"#F16A26"}}>
+         MarketPlace
+      </Text>
       </Pressable>
 
       <Pressable onPress={ () => navigation.navigate("Cart")} className="h-12 w-12 rounded-full bg-gray-100 flex items-center justify-center">
