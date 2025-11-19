6d66763a08480cb66c4140dfab43187c07be096a
MostafaDodo <mostafa99amr@gmail.com>
2025-11-19 23:58:59 +0200

    fixes

--- App.js
+++ App.js
@@ -1,25 +1,29 @@
-import React, { useEffect } from 'react';
-import { Provider, useDispatch } from 'react-redux';
-import "./global.css";
-import AppNavigator from './src/navigation/AppNavigator';
-import store from './src/redux/store';
+import { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
+import { Provider, useDispatch } from "react-redux";
+import "./global.css";
 import { CustomToast } from "./src/components/CustomToast";
-import { loadWishlistFromStorage } from './src/redux/slices/wishlistSlice';
-// استيراد الـ store و الـ Provider من react-redux
+import AppNavigator from "./src/navigation/AppNavigator";
+import { loadWishlistFromStorage } from "./src/redux/slices/wishlistSlice";
+import store from "./src/redux/store";
+
 function AppContent() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-// تحميل الـ wishlist من التخزين المحلي عند بدء التطبيق
+    // تحميل الـ wishlist من التخزين المحلي عند بدء التطبيق
     dispatch(loadWishlistFromStorage());
   }, [dispatch]);
 
   return (
-    <>
-      <AppNavigator />
-      <Toast config={CustomToast} />
-    </>
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <AppNavigator />
+        <Toast config={CustomToast} />
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
 
