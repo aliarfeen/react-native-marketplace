c86c15debfcd803461cb439700deab626be5a95b
Ali Hesham <63455367+aliarfeen@users.noreply.github.com>
2025-11-21 21:53:04 +0200

    updated store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+
+import authReducer from "./slices/authSlice";
 import cartReducer from "./slices/cartSlice";
 import homeReducer from "./slices/homeSlice";
+import wishlistReducer from "./slices/wishlistSlice";
 
 const store = configureStore({
   reducer: {
     home: homeReducer,
+    auth: authReducer,
+    wishlist: wishlistReducer,
     cart: cartReducer,
   },
 });
 
 export default store;
-git;
